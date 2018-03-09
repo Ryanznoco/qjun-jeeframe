47708f2ea31849fd1392229b75bac23fce1f2bdf
RenQiang <810625938@qq.com>
2018-03-09 18:45:11 +0800

    添加jeeframe-core模块

---
+++ src/frame/core/src/test/java/cn/com/qjun/MethodTest.java
@@ -0,0 +1,15 @@
+package cn.com.qjun;
+
+import org.hibernate.id.UUIDGenerator;
+import org.junit.Test;
+
+/**
+ * @author RenQiang
+ * @date 2018/3/9
+ */
+public class MethodTest {
+
+    @Test
+    public void uuidTest() {
+    }
+}
