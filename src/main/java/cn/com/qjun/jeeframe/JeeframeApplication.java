f7c2371145d27f3c49a6c7ab74cffc608f5d0f63
RenQiang <810625938@qq.com>
2018-03-10 15:19:40 +0800

    初始导入

---
+++ src/main/java/cn/com/qjun/jeeframe/JeeframeApplication.java
@@ -0,0 +1,12 @@
+package cn.com.qjun.jeeframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeframeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeeframeApplication.class, args);
+    }
+}
